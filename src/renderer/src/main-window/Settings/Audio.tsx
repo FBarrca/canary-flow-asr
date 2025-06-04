7336badd8eafc74264f712b632b217fa0f65a4c1
Barrca <60709314+FBarrca@users.noreply.github.com>
2025-06-04 23:32:52 +0200

    Add audio input device selection to settings

--- src/renderer/src/main-window/Settings/Audio.tsx
+++ src/renderer/src/main-window/Settings/Audio.tsx
@@ -1,13 +1,45 @@
-
-
-
 import React from "react";
-import { Label, Dropdown, Option, Slider, Switch } from "@fluentui/react-components";
+import {
+  Label,
+  Dropdown,
+  Option,
+  Slider,
+  Switch,
+} from "@fluentui/react-components";
 import { Mic24Regular } from "@fluentui/react-icons";
 import SectionCard from "./Sectioncard";
 import Row from "./Row";
 
 const AudioSettings: React.FC = () => {
+  const [devices, setDevices] = React.useState<MediaDeviceInfo[]>([]);
+  const [selectedDevice, setSelectedDevice] = React.useState<string | undefined>();
+
+  React.useEffect(() => {
+    async function loadDevices() {
+      try {
+        // Request permission so device labels are populated
+        await navigator.mediaDevices.getUserMedia({ audio: true });
+      } catch {
+        // Permission denied or not required
+      }
+
+      try {
+        const devs = await navigator.mediaDevices.enumerateDevices();
+        setDevices(devs.filter((d) => d.kind === "audioinput"));
+      } catch {
+        setDevices([]);
+      }
+    }
+
+    loadDevices();
+    navigator.mediaDevices.addEventListener("devicechange", loadDevices);
+    return () => navigator.mediaDevices.removeEventListener("devicechange", loadDevices);
+  }, []);
+
+  const handleSelect = (_: any, data: any) => {
+    setSelectedDevice(data.optionValue as string);
+  };
+
   return (
     <SectionCard
     icon={<Mic24Regular />}
@@ -17,11 +49,17 @@ const AudioSettings: React.FC = () => {
     <div style={{ padding: 24, display: "flex", flexDirection: "column", gap: 24 }}>
       <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
         <Label htmlFor="input-device">Input Device</Label>
-        <Dropdown placeholder="Select microphone" style={{ width: 280 }}>
-          <Option value="default">Default Microphone</Option>
-          <Option value="usb-mic">USB Microphone (Blue Yeti)</Option>
-          <Option value="headset">Headset Microphone</Option>
-          <Option value="internal">Internal Microphone</Option>
+        <Dropdown
+          placeholder="Select microphone"
+          style={{ width: 280 }}
+          value={selectedDevice}
+          onOptionSelect={handleSelect}
+        >
+          {devices.map((device, idx) => (
+            <Option key={device.deviceId} value={device.deviceId}>
+              {device.label || `Microphone ${idx + 1}`}
+            </Option>
+          ))}
         </Dropdown>
       </div>
 
